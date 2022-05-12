40cd0896bf61e671918880e25c87f62db313f561
hugosenari <hugosenari@gmail.com>
2022-05-12 11:51:09 -0300

    chore: re index drand48

--- flake.nix
+++ flake.nix
@@ -7,6 +7,54 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."drand48-master".dir   = "master";
+  inputs."drand48-master".owner = "nim-nix-pkgs";
+  inputs."drand48-master".ref   = "master";
+  inputs."drand48-master".repo  = "drand48";
+  inputs."drand48-master".type  = "github";
+  inputs."drand48-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."drand48-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."drand48-v1_0_0".dir   = "v1_0_0";
+  inputs."drand48-v1_0_0".owner = "nim-nix-pkgs";
+  inputs."drand48-v1_0_0".ref   = "master";
+  inputs."drand48-v1_0_0".repo  = "drand48";
+  inputs."drand48-v1_0_0".type  = "github";
+  inputs."drand48-v1_0_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."drand48-v1_0_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."drand48-v1_0_1".dir   = "v1_0_1";
+  inputs."drand48-v1_0_1".owner = "nim-nix-pkgs";
+  inputs."drand48-v1_0_1".ref   = "master";
+  inputs."drand48-v1_0_1".repo  = "drand48";
+  inputs."drand48-v1_0_1".type  = "github";
+  inputs."drand48-v1_0_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."drand48-v1_0_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."drand48-v1_1_0".dir   = "v1_1_0";
+  inputs."drand48-v1_1_0".owner = "nim-nix-pkgs";
+  inputs."drand48-v1_1_0".ref   = "master";
+  inputs."drand48-v1_1_0".repo  = "drand48";
+  inputs."drand48-v1_1_0".type  = "github";
+  inputs."drand48-v1_1_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."drand48-v1_1_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."drand48-v1_1_1".dir   = "v1_1_1";
+  inputs."drand48-v1_1_1".owner = "nim-nix-pkgs";
+  inputs."drand48-v1_1_1".ref   = "master";
+  inputs."drand48-v1_1_1".repo  = "drand48";
+  inputs."drand48-v1_1_1".type  = "github";
+  inputs."drand48-v1_1_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."drand48-v1_1_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."drand48-v1_1_2".dir   = "v1_1_2";
+  inputs."drand48-v1_1_2".owner = "nim-nix-pkgs";
+  inputs."drand48-v1_1_2".ref   = "master";
+  inputs."drand48-v1_1_2".repo  = "drand48";
+  inputs."drand48-v1_1_2".type  = "github";
+  inputs."drand48-v1_1_2".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."drand48-v1_1_2".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
